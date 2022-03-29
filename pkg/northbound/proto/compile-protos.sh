bde4b120ed08bec5a93c25ea9d10bf3387d61bce
Ray Milkey <ray.milkey@gmail.com>
2022-03-29 10:49:37 -0700

    Enable reuse tool

--- pkg/northbound/proto/compile-protos.sh
+++ pkg/northbound/proto/compile-protos.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 proto_imports=".:${GOPATH}/src/github.com/google/protobuf/src:${GOPATH}/src"
 
 protoc -I=$proto_imports --go_out=plugins=grpc:. pkg/northbound/proto/*.proto
