5ad14e229ff5e44b139c14c019368e3b9c5320ad
Thomas Vachuska <tom.vachuska@intel.com>
2023-01-24 09:33:30 -0800

    Sketching out stores layout (#25)

--- go.mod
+++ go.mod
@@ -5,19 +5,54 @@ go 1.19
 require (
 	github.com/atomix/go-sdk v0.10.0
 	github.com/onosproject/onos-api/go v0.10.21
+	github.com/onosproject/onos-lib-go v0.10.6
 	github.com/p4lang/p4runtime v1.3.0
 )
 
 require (
+	github.com/Shopify/sarama v1.31.1 // indirect
+	github.com/atomix/runtime/sdk v0.7.4 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eapache/go-resiliency v1.2.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20180814174437-776d5712da21 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/hashicorp/go-uuid v1.0.2 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.0.0 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.2 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/klauspost/compress v1.14.2 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.0-beta.8 // indirect
+	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.11.0 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
 	golang.org/x/net v0.0.0-20220412020605-290c469a71a5 // indirect
 	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/xerrors v0.0.0-20220411194840-2f41105eb62f // indirect
 	google.golang.org/genproto v0.0.0-20220407144326-9054f6ed7bac // indirect
 	google.golang.org/grpc v1.46.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- pkg/store/stores.go
+++ pkg/store/stores.go
@@ -6,16 +6,23 @@
 package store
 
 import (
+	"context"
 	"github.com/atomix/go-sdk/pkg/primitive"
 	"github.com/onosproject/onos-api/go/onos/topo"
+	"github.com/onosproject/onos-lib-go/pkg/errors"
+	"github.com/onosproject/onos-lib-go/pkg/logging"
+	p4info "github.com/p4lang/p4runtime/go/p4/config/v1"
+	"sync"
 )
+
+var log = logging.GetLogger("store")
 
 // Stores is an abstraction of an entity capable of tracking multiple entity stores created
 // on behalf of different devices.
 type Stores interface {
 	// Get returns an entity store for the specified device. A new one will be created if it doesn't
 	// already exist. Returns error if the backing storage system is unavailable.
-	Get(id topo.ID) (EntityStore, error)
+	Get(ctx context.Context, id topo.ID, info *p4info.P4Info) (EntityStore, error)
 
 	// GetAll returns the list of all currently registered entity stores. It returns only those store
 	// that have been retrieved and/or implicitly created via retrieval during this run-time, meaning
@@ -23,11 +30,12 @@ type Stores interface {
 	GetAll() []EntityStore
 
 	// Purge purges the given device entity store and all the data within it.
-	Purge(id topo.ID) error
+	Purge(ctx context.Context, id topo.ID) error
 }
 
 type storeManager struct {
 	Stores
+	mu     sync.RWMutex
 	stores map[topo.ID]EntityStore
 }
 
@@ -38,14 +46,40 @@ func NewStoreManager(client primitive.Client) Stores {
 }
 
 // Get returns an entity store for the specified device.
-func (sm *storeManager) Get(id topo.ID) (EntityStore, error) {
-	// TODO: Implement me
-	return nil, nil
+func (sm *storeManager) Get(ctx context.Context, id topo.ID, info *p4info.P4Info) (EntityStore, error) {
+	sm.mu.RLock()
+	store, ok := sm.stores[id]
+	defer sm.mu.RUnlock()
+	if ok {
+		return store, nil
+	}
+
+	if info == nil {
+		return nil, errors.NewNotFound("Store not found for %s", id)
+	}
+
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+
+	store, ok = sm.stores[id]
+	if ok {
+		return store, nil
+	}
+
+	var err error
+	store, err = newEntityStore(id, info)
+	if err != nil {
+		return nil, errors.FromAtomix(err)
+	}
+	sm.stores[id] = store
+	return store, nil
 }
 
 // GetAll returns the list of all currently registered entity stores.
 func (sm *storeManager) GetAll() []EntityStore {
 	stores := make([]EntityStore, 0, len(sm.stores))
+	sm.mu.RLock()
+	defer sm.mu.RUnlock()
 	for _, store := range sm.stores {
 		stores = append(stores, store)
 	}
@@ -53,7 +87,12 @@ func (sm *storeManager) GetAll() []EntityStore {
 }
 
 // Purge purges the given device entity store and all the data within it.
-func Purge(id topo.ID) error {
+func (sm *storeManager) Purge(ctx context.Context, id topo.ID) error {
+	store, err := sm.Get(ctx, id, nil)
+	if err != nil {
+		return err
+	}
+	log.Infof("Purging store %s: %+v", id, store)
 	// TODO: Implement me
 	return nil
 }
